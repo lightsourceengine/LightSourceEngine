7304ad6907cdd7522f67ce2c07d03299dab63176
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2019-10-04 10:37:18 -0700

    Turn on TGA and PSD image support

--- packages/light-source/addon/deps/stb/src/stb_image.cc
+++ packages/light-source/addon/deps/stb/src/stb_image.cc
@@ -5,11 +5,9 @@
  */
 
 #define STBI_NO_FAILURE_STRINGS
-#define STBI_NO_PSD
 #define STBI_NO_PIC
 #define STBI_NO_PNM
 #define STBI_NO_HDR
-#define STBI_NO_TGA
 #define STBI_NO_LINEAR
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
