ab2de9d0de75d1b1f2a1241af130d3cf06c7af64
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-09-16 13:41:24 -0700

    Fix upx caching issue (why?)

--- scripts/ci/get-upx.sh
+++ scripts/ci/get-upx.sh
@@ -3,7 +3,5 @@
 set -e
 set -x
 
-if [ ! -d "${HOME}/upx" ]; then
-  wget -qO- https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ -C "${HOME}"
-  mv "${HOME}/upx-3.96-amd64_linux" "${HOME}/upx"
-fi
+wget -qO- https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ -C "${HOME}"
+mv "${HOME}/upx-3.96-amd64_linux" "${HOME}/upx"
