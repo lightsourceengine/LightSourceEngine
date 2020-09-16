fa8cb4077f93cb0a16026e5934ffefbae836abb8
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-09-16 11:58:20 -0700

    Add logging to debug build issue (cannot find crosstools home)

--- scripts/ci/get-upx.sh
+++ scripts/ci/get-upx.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+set -x
+
 if [ ! -d "${HOME}/upx" ]; then
   wget -qO- https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ -C "${HOME}"
   mv "${HOME}/upx-3.96-amd64_linux" "${HOME}/upx"
--- scripts/ci/lse-git-clone.sh
+++ scripts/ci/lse-git-clone.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+set -e
+set -x
+
 for repo in "$@"
 do
   if [ ! -d "${HOME}/${repo}" ]; then
     git clone --depth 1 https://github.com/lightsourceengine/${repo}.git "${HOME}/${repo}"
+
+    if [ ! -d "${HOME}/${repo}" ]; then
+      echo "Failed to clone ${repo}."
+      exit 1
+    fi
   fi
 done
