dc02830f9b990bd1aa9e6a2e7e1672b13966017d
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-09-16 13:54:39 -0700

    Fix incorrect variable name

--- scripts/ci/get-upx.sh
+++ scripts/ci/get-upx.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-if [ ! -d "${LS_CACHE}/upx" ]; then
+if [ ! -d "${LSE_CACHE}/upx" ]; then
   wget -qO- https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ -C "${HOME}"
-  mv "${HOME}/upx-3.96-amd64_linux" "${LS_CACHE}/upx"
+  mv "${HOME}/upx-3.96-amd64_linux" "${LSE_CACHE}/upx"
 fi
--- scripts/ci/lse-git-clone.sh
+++ scripts/ci/lse-git-clone.sh
@@ -5,7 +5,7 @@ set -x
 
 for repo in "$@"
 do
-  if [ ! -d "${LS_CACHE}/${repo}" ]; then
-    git clone --depth 1 https://github.com/lightsourceengine/${repo}.git "${LS_CACHE}/${repo}"
+  if [ ! -d "${LSE_CACHE}/${repo}" ]; then
+    git clone --depth 1 https://github.com/lightsourceengine/${repo}.git "${LSE_CACHE}/${repo}"
   fi
 done
