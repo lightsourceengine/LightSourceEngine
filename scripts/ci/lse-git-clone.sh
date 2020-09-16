8e4c7b08fc3c076bd4c53cd8dbb4ab58372fa326
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-09-16 12:53:56 -0700

    Fix a caching issue with ci-pacakges and crosstools

--- scripts/ci/lse-git-clone.sh
+++ scripts/ci/lse-git-clone.sh
@@ -5,12 +5,5 @@ set -x
 
 for repo in "$@"
 do
-  if [ ! -d "${HOME}/${repo}" ]; then
-    git clone --depth 1 https://github.com/lightsourceengine/${repo}.git "${HOME}/${repo}"
-
-    if [ ! -d "${HOME}/${repo}" ]; then
-      echo "Failed to clone ${repo}."
-      exit 1
-    fi
-  fi
+  git clone --depth 1 https://github.com/lightsourceengine/${repo}.git "${HOME}/${repo}"
 done
