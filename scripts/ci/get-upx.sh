56d17b82b69613e74665990cecbfa3081b9c88e4
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-09-16 13:50:51 -0700

    Fix the cache problems (also dealing with network issues on the ci machines)

--- scripts/ci/get-upx.sh
+++ scripts/ci/get-upx.sh
@@ -3,5 +3,7 @@
 set -e
 set -x
 
-wget -qO- https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ -C "${HOME}"
-mv "${HOME}/upx-3.96-amd64_linux" "${HOME}/upx"
+if [ ! -d "${LS_CACHE}/upx" ]; then
+  wget -qO- https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ -C "${HOME}"
+  mv "${HOME}/upx-3.96-amd64_linux" "${LS_CACHE}/upx"
+fi
--- scripts/ci/lse-git-clone.sh
+++ scripts/ci/lse-git-clone.sh
@@ -5,5 +5,7 @@ set -x
 
 for repo in "$@"
 do
-  git clone --depth 1 https://github.com/lightsourceengine/${repo}.git "${HOME}/${repo}"
+  if [ ! -d "${LS_CACHE}/${repo}" ]; then
+    git clone --depth 1 https://github.com/lightsourceengine/${repo}.git "${LS_CACHE}/${repo}"
+  fi
 done
