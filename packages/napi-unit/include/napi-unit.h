33a452a31ee2fb3aaef33bf9947916456f105252
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2020-12-20 21:34:10 -0800

    Fix lint errors. Reformat to tab size of 2.

--- packages/napi-unit/include/napi-unit.h
+++ packages/napi-unit/include/napi-unit.h
@@ -13,6 +13,7 @@
 #include <functional>
 #include <initializer_list>
 #include <sstream>
+#include <memory>
 
 namespace Napi {
 
@@ -29,8 +30,8 @@ using TestBuilderFunction = std::function<void(TestSuite*)>;
  * Maps to parameters of Mocha's test declaration: it(description, function).
  */
 struct Test {
-    std::string description;
-    TestFunction func;
+  std::string description;
+  TestFunction func;
 };
 
 /**
@@ -38,19 +39,19 @@ struct Test {
  */
 class TestInfo {
  public:
-    TestInfo(napi_env env) : env(env) {}
+  TestInfo(napi_env env) : env(env) {}
 
-    Napi::Env Env() const noexcept { return env; }
+  Napi::Env Env() const noexcept { return env; }
 
  private:
-    napi_env env;
+  napi_env env;
 };
 
 /**
  * Interface for data owned by a TestSuite and shared between test cases.
  */
 class TestContext {
-    virtual ~TestContext() = default;
+  virtual ~TestContext() = default;
 };
 
 /**
@@ -61,57 +62,57 @@ class TestContext {
  */
 class TestSuite {
  public:
-    explicit TestSuite(const std::string& description);
+  explicit TestSuite(const std::string& description);
 
-    /**
-     * Create a new TestSuite and populate it with tests using a list TestBuilderFunctions.
-     */
-    static Napi::Object Build(Napi::Env env, const std::string& description,
-        const std::initializer_list<TestBuilderFunction> testBuilders);
+  /**
+   * Create a new TestSuite and populate it with tests using a list TestBuilderFunctions.
+   */
+  static Napi::Object Build(Napi::Env env, const std::string& description,
+                            const std::initializer_list<TestBuilderFunction> testBuilders);
 
-    /**
-     * Create a new child TestSuite.
-     */
-    TestSuite* Describe(const std::string& description);
+  /**
+   * Create a new child TestSuite.
+   */
+  TestSuite* Describe(const std::string& description);
 
-    /**
-     * Get TestContext as a test specific subclass.
-     */
-    template<class T>
-    std::shared_ptr<T> GetContextAs() {
-        return std::static_pointer_cast<T>(this->context).get();
-    }
+  /**
+   * Get TestContext as a test specific subclass.
+   */
+  template<class T>
+  std::shared_ptr<T> GetContextAs() {
+    return std::static_pointer_cast<T>(this->context).get();
+  }
 
  public:
-    // Note: These are exposed publicly for test writing convenience.
+  // Note: These are exposed publicly for test writing convenience.
 
-    // List of test cases. Optional.
-    std::vector<Test> tests;
-    // Function executed before tests are run. Optional.
-    TestSuiteFunction before;
-    // Function executed after all tests are run. Optional.
-    TestSuiteFunction after;
-    // Function executed before each test case is run. Optional.
-    TestSuiteFunction beforeEach;
-    // Function executed after each test case is run. Optional.
-    TestSuiteFunction afterEach;
-    // Test specific data attached this test suite. Optional.
-    std::shared_ptr<TestContext> context;
-
- private:
-    Napi::Object ToObject(const Napi::Env& env);
-    Napi::Value GetDescription(const Napi::Env& env);
-    Napi::Value GetTests(const Napi::Env& env);
-    Napi::Value GetChildren(const Napi::Env& env);
-    Napi::Value GetBefore(const Napi::Env& env);
-    Napi::Value GetAfter(const Napi::Env& env);
-    Napi::Value GetBeforeEach(const Napi::Env& env);
-    Napi::Value GetAfterEach(const Napi::Env& env);
-    Napi::Value TestSuiteFunctionOrUndefined(const Napi::Env& env, TestSuiteFunction func);
+  // List of test cases. Optional.
+  std::vector<Test> tests;
+  // Function executed before tests are run. Optional.
+  TestSuiteFunction before;
+  // Function executed after all tests are run. Optional.
+  TestSuiteFunction after;
+  // Function executed before each test case is run. Optional.
+  TestSuiteFunction beforeEach;
+  // Function executed after each test case is run. Optional.
+  TestSuiteFunction afterEach;
+  // Test specific data attached this test suite. Optional.
+  std::shared_ptr<TestContext> context;
 
  private:
-    std::string description;
-    std::vector<TestSuite> children;
+  Napi::Object ToObject(const Napi::Env& env);
+  Napi::Value GetDescription(const Napi::Env& env);
+  Napi::Value GetTests(const Napi::Env& env);
+  Napi::Value GetChildren(const Napi::Env& env);
+  Napi::Value GetBefore(const Napi::Env& env);
+  Napi::Value GetAfter(const Napi::Env& env);
+  Napi::Value GetBeforeEach(const Napi::Env& env);
+  Napi::Value GetAfterEach(const Napi::Env& env);
+  Napi::Value TestSuiteFunctionOrUndefined(const Napi::Env& env, TestSuiteFunction func);
+
+ private:
+  std::string description;
+  std::vector<TestSuite> children;
 };
 
 /**
@@ -120,19 +121,19 @@ class TestSuite {
  */
 class AssertionError : public std::exception {
  public:
-    AssertionError(const std::string& failure, const std::string& message) noexcept;
-    virtual ~AssertionError() noexcept = default;
+  AssertionError(const std::string& failure, const std::string& message) noexcept;
+  virtual ~AssertionError() noexcept = default;
 
-    const char* what() const noexcept override {
-        return this->error.c_str();
-    }
+  const char* what() const noexcept override {
+    return this->error.c_str();
+  }
 
-    const std::string& GetError() const noexcept {
-        return this->error;
-    }
+  const std::string& GetError() const noexcept {
+    return this->error;
+  }
 
  private:
-    std::string error;
+  std::string error;
 };
 
 /**
@@ -141,116 +142,116 @@ class AssertionError : public std::exception {
  * To be compatible with the javascript test environment, assertion errors are thrown using napi exceptions.
  */
 struct Assert {
-    /**
-     * Checks that two values are equal using the == operator.
-     */
-    template<typename S, typename T>
-    static void Equal(const S& value, const T& expected, const std::string& message = "") {
-        if (!(value == expected)) {
-            std::stringstream ss;
+  /**
+   * Checks that two values are equal using the == operator.
+   */
+  template<typename S, typename T>
+  static void Equal(const S& value, const T& expected, const std::string& message = "") {
+    if (!(value == expected)) {
+      std::stringstream ss;
 
-            ss << "Expected " << value << " to be equal to " << expected;
+      ss << "Expected " << value << " to be equal to " << expected;
 
-            throw AssertionError(ss.str(), message);
-        }
+      throw AssertionError(ss.str(), message);
+    }
+  }
+
+  static void CStringEqual(const char* value, const char* expected, const std::string& message = "") {
+    if (value == nullptr && expected == nullptr) {
+      return;
+    } else if (value != nullptr && expected != nullptr && strcmp(value, expected) == 0) {
+      return;
     }
 
-    static void CStringEqual(const char* value, const char* expected, const std::string& message = "") {
-        if (value == nullptr && expected == nullptr) {
-            return;
-        } else if (value != nullptr && expected != nullptr && strcmp(value, expected) == 0) {
-            return;
-        }
+    std::stringstream ss;
 
-        std::stringstream ss;
+    ss << "Expected " << (value ? value : "null") << " to be equal to " << (expected ? expected : "null");
 
-        ss << "Expected " << (value ? value : "null") << " to be equal to " << (expected ? expected : "null");
+    throw AssertionError(ss.str(), message);
+  }
 
-        throw AssertionError(ss.str(), message);
+  /**
+   * Checks that the value is equal to nullptr.
+   */
+  template<typename T>
+  static void IsNull(const T& value, const std::string& message = "") {
+    if (!(value == nullptr)) {
+      std::stringstream ss;
+
+      ss << "Expected " << value << " to be nullptr";
+
+      throw AssertionError(ss.str(), message);
+    }
+  }
+
+  /**
+   * Checks that the value is not equal to nullptr.
+   */
+  template<typename T>
+  static void IsNotNull(const T& value, const std::string& message = "") {
+    if (value == nullptr) {
+      std::stringstream ss;
+
+      ss << "Expected " << value << " to not be nullptr";
+
+      throw AssertionError(ss.str(), message);
+    }
+  }
+
+  /**
+   * Checks that the value is true.
+   */
+  static void IsTrue(const bool value, const std::string& message = "") {
+    if (!value) {
+      throw AssertionError("Expected true", message);
+    }
+  }
+
+  /**
+   * Checks that the value is false.
+   */
+  static void IsFalse(const bool value, const std::string& message = "") {
+    if (value) {
+      throw AssertionError("Expected false", message);
+    }
+  }
+
+  /**
+   * Checks that calling the passed in lambda function will throw an std::exception.
+   */
+  static void Throws(std::function<void()> func, const std::string& message = "") {
+    try {
+      func();
+    } catch (const std::exception&) {
+      return;
     }
 
-    /**
-     * Checks that the value is equal to nullptr.
-     */
-    template<typename T>
-    static void IsNull(const T& value, const std::string& message = "") {
-        if (!(value == nullptr)) {
-            std::stringstream ss;
+    throw AssertionError("Expected function to throw an std::exception.", message);
+  }
 
-            ss << "Expected " << value << " to be nullptr";
-
-            throw AssertionError(ss.str(), message);
-        }
-    }
-
-    /**
-     * Checks that the value is not equal to nullptr.
-     */
-    template<typename T>
-    static void IsNotNull(const T& value, const std::string& message = "") {
-        if (value == nullptr) {
-            std::stringstream ss;
-
-            ss << "Expected " << value << " to not be nullptr";
-
-            throw AssertionError(ss.str(), message);
-        }
-    }
-
-    /**
-     * Checks that the value is true.
-     */
-    static void IsTrue(const bool value, const std::string& message = "") {
-        if (!value) {
-            throw AssertionError("Expected true", message);
-        }
-    }
-
-    /**
-     * Checks that the value is false.
-     */
-    static void IsFalse(const bool value, const std::string& message = "") {
-        if (value) {
-            throw AssertionError("Expected false", message);
-        }
-    }
-
-    /**
-     * Checks that calling the passed in lambda function will throw an std::exception.
-     */
-    static void Throws(std::function<void()> func, const std::string& message = "") {
-        try {
-            func();
-        } catch (const std::exception&) {
-            return;
-        }
-
-        throw AssertionError("Expected function to throw an std::exception.", message);
-    }
-
-    /**
-     * Fail the test immediately by throwing an exception.
-     */
-    static void Fail(const std::string& message = "Fail") {
-        throw AssertionError("", message);
-    }
+  /**
+   * Fail the test immediately by throwing an exception.
+   */
+  static void Fail(const std::string& message = "Fail") {
+    throw AssertionError("", message);
+  }
 };
 
 inline
 AssertionError::AssertionError(const std::string& failure, const std::string& message) noexcept {
-    std::stringstream ss;
+  std::stringstream ss;
 
-    ss << "AssertionError";
+  ss << "AssertionError";
 
-    if (!failure.empty()) {
-        ss << ": " << failure;
-    }
+  if (!failure.empty()) {
+    ss << ": " << failure;
+  }
 
-    if (!message.empty()) {
-        ss << ": " << message;
-    }
+  if (!message.empty()) {
+    ss << ": " << message;
+  }
 
-    this->error = ss.str();
+  this->error = ss.str();
 }
 
 inline
@@ -259,117 +260,117 @@ TestSuite::TestSuite(const std::string& description) : description(description) 
 
 inline
 Object TestSuite::Build(Napi::Env env, const std::string& description,
-        const std::initializer_list<TestBuilderFunction> testBuilders) {
-    TestSuite parent(description);
+                        const std::initializer_list<TestBuilderFunction> testBuilders) {
+  TestSuite parent(description);
 
-    for (auto& testBuilder : testBuilders) {
-        testBuilder(&parent);
-    }
+  for (auto& testBuilder : testBuilders) {
+    testBuilder(&parent);
+  }
 
-    return parent.ToObject(env);
+  return parent.ToObject(env);
 }
 
 inline
 Napi::Object TestSuite::ToObject(const Napi::Env& env) {
-    auto obj{ Object::New(env) };
+  auto obj{Object::New(env)};
 
-    obj.Set("description", this->GetDescription(env));
-    obj.Set("before", this->GetBefore(env));
-    obj.Set("after", this->GetAfter(env));
-    obj.Set("beforeEach", this->GetBeforeEach(env));
-    obj.Set("afterEach", this->GetAfterEach(env));
-    obj.Set("tests", this->GetTests(env));
-    obj.Set("children", this->GetChildren(env));
+  obj.Set("description", this->GetDescription(env));
+  obj.Set("before", this->GetBefore(env));
+  obj.Set("after", this->GetAfter(env));
+  obj.Set("beforeEach", this->GetBeforeEach(env));
+  obj.Set("afterEach", this->GetAfterEach(env));
+  obj.Set("tests", this->GetTests(env));
+  obj.Set("children", this->GetChildren(env));
 
-    return obj;
+  return obj;
 }
 
 inline
 Napi::Value TestSuite::GetDescription(const Napi::Env& env) {
-    return String::New(env, this->description);
+  return String::New(env, this->description);
 }
 
 inline
 Napi::Value TestSuite::GetTests(const Napi::Env& env) {
-    auto result{ Array::New(env, this->tests.size()) };
-    auto i{ 0u };
+  auto result{Array::New(env, this->tests.size())};
+  auto i{0u};
 
-    for (auto& test : this->tests) {
-        auto object{ Object::New(env) };
+  for (auto& test : this->tests) {
+    auto object{Object::New(env)};
 
-        auto jsSafeTestFunc = [f = test.func](const Napi::CallbackInfo& info) {
-            auto env{ info.Env() };
-            HandleScope scope(env);
-            const TestInfo testInfo(env);
+    auto jsSafeTestFunc = [f = test.func](const Napi::CallbackInfo& info) {
+      auto env{info.Env()};
+      HandleScope scope(env);
+      const TestInfo testInfo(env);
 
-            try {
-                f(testInfo);
-            } catch (const AssertionError& e) {
-                throw Error::New(env, e.what());
-            } catch (const Error&) {
-                throw;
-            } catch (const std::exception& e) {
-                throw Error::New(env, std::string("Unknown std::exception: ") + e.what());
-            } catch (...) {
-                throw Error::New(env, "Unknown native exception!");
-            }
-        };
+      try {
+        f(testInfo);
+      } catch (const AssertionError& e) {
+        throw Error::New(env, e.what());
+      } catch (const Error&) {
+        throw;
+      } catch (const std::exception& e) {
+        throw Error::New(env, std::string("Unknown std::exception: ") + e.what());
+      } catch (...) {
+        throw Error::New(env, "Unknown native exception!");
+      }
+    };
 
-        object["description"] = String::New(env, test.description);
-        object["func"] = Function::New(env, jsSafeTestFunc);
+    object["description"] = String::New(env, test.description);
+    object["func"] = Function::New(env, jsSafeTestFunc);
 
-        result[i++] = object;
-    }
+    result[i++] = object;
+  }
 
-    return result;
+  return result;
 }
 
 inline
 Napi::Value TestSuite::GetChildren(const Napi::Env& env) {
-    auto result{ Array::New(env, this->children.size()) };
-    auto i{ 0u };
+  auto result{Array::New(env, this->children.size())};
+  auto i{0u};
 
-    for (auto& child : this->children) {
-        result[i++] = child.ToObject(env);
-    }
+  for (auto& child : this->children) {
+    result[i++] = child.ToObject(env);
+  }
 
-    return result;
+  return result;
 }
 
 inline
 Napi::Value TestSuite::GetBefore(const Napi::Env& env) {
-    return TestSuiteFunctionOrUndefined(env, this->before);
+  return TestSuiteFunctionOrUndefined(env, this->before);
 }
 
 inline
 Napi::Value TestSuite::GetAfter(const Napi::Env& env) {
-    return TestSuiteFunctionOrUndefined(env, this->after);
+  return TestSuiteFunctionOrUndefined(env, this->after);
 }
 
 inline
 Napi::Value TestSuite::GetBeforeEach(const Napi::Env& env) {
-    return TestSuiteFunctionOrUndefined(env, this->beforeEach);
+  return TestSuiteFunctionOrUndefined(env, this->beforeEach);
 }
 
 inline
 Napi::Value TestSuite::GetAfterEach(const Napi::Env& env) {
-    return TestSuiteFunctionOrUndefined(env, this->afterEach);
+  return TestSuiteFunctionOrUndefined(env, this->afterEach);
 }
 
 inline
 TestSuite* TestSuite::Describe(const std::string& description) {
-    this->children.emplace_back(description);
+  this->children.emplace_back(description);
 
-    return &this->children.back();
+  return &this->children.back();
 }
 
 inline
 Napi::Value TestSuite::TestSuiteFunctionOrUndefined(const Napi::Env& env, TestSuiteFunction func) {
-    if (func) {
-        return Function::New(env, [func](const Napi::CallbackInfo& info) { func(info.Env()); });
-    }
+  if (func) {
+    return Function::New(env, [func](const Napi::CallbackInfo& info) { func(info.Env()); });
+  }
 
-    return env.Undefined();
+  return env.Undefined();
 }
 
 } // namespace Napi
